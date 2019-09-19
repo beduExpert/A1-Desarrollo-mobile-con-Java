f5d56018c86b8670f095904620eccfa19f923690
antonioarista <antonio.arista@gintelligence.mx>
2019-09-19 15:52:09 -0500

    Este es el ejemplo de la sesión 01. No hay demasiado código que ver Sin embargo este será el punto de partida de las demás sesiones.
    Buena suerte!

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //En este ejemplo no hay mucho que hacer, mas que explorar :D
+    //Revisa cada uno de los folders, y checa cada elemento de esta actividad :)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
