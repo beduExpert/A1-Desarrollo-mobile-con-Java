d2199741926c808558fa99815f7464f2df62012d
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 18:39:12 -0500

    En este ejemplo mostramos como funcionan los métodos en Java

---
+++ SourceCode/app/src/main/java/com/bedu/pokedex/Pokemon.java
@@ -0,0 +1,27 @@
+package com.bedu.pokedex;
+
+import android.util.Log;
+
+/**
+ * Pokemon.java :D
+ */
+public class Pokemon {
+    //Aqui escribiremos el programa que va a controlar a todos los Pokemon.
+    //Primero todos los datos sobre un pokemon.
+
+    int numero;             //Numero (valor entero)
+    String nombre;          //Nombre (Cadena de caracteres)
+    double altura;          //Altura (valor de punto flotante)
+    double peso;            //Peso (valor de punto flotante)
+    String descripcion;     //Descripción (cadena de caracteres)
+    int tipo;               //Tipo del pokemon (valor entero).
+
+    int HP;                 //Este es el HP, lo cabo de agregar para este ejemplo.
+
+    //Este es el método (función) de atacar:
+    int atacar (String nombreAtaque)
+    {
+        Log.d("PokeDex",nombre+ "Ha utilizado "+nombreAtaque);
+        return 10;
+    }
+}
