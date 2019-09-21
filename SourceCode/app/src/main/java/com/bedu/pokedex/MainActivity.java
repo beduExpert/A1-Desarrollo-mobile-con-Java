b8a391a55e2ec6bdbe211e856a70346e34027e05
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 16:56:12 -0500

    Este ejemplo nuestra como funcionan dos componentes: Un EditText y un botón.
    Puedes agregar mas componentes en este ejemplo si lo necesitas :)
    ¡Buena suerte!

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Revisa el archivo activity_main.xml
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
