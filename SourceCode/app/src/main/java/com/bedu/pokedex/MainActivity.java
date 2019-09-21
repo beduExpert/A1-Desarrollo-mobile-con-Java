19d49335fe18c70d174672e7cbb51f18ac7b757d
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 16:00:15 -0500

    Este es el primer ejemplo de un LinearLayout: Tiene ancho (layout_width), alto (layout_height) orientación y gravedad.

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Para este ejemplo, consulta el archivo res/layout/activity_main.xml
+    //Ese archivo controla la interfaz gráfica.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
