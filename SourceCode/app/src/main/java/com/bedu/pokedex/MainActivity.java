5c98a54f463d4db0d973ae4c80ed7a02a070dca6
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 18:57:11 -0500

    Este es un ejemplo de como se usa un constructor dentro de Java Buena suerte con los retos!

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -10,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Aqui vemos como se usa un constructor:
+        //Nota que puse cada parámetro del constructor en una línea diferente para que sea mas legible
+        
+        Pokemon charmander = new Pokemon(
+                1,
+                "Charmander",
+                30,
+                2.0,
+                "Su colita está en llamas",
+                6,
+                40);
     }
 }
