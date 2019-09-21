dee04bbfc23eefec782c6df410d44b834afc7d93
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 18:11:16 -0500

    Este es el primer ejemplo de la sesión 4. Esta es una clase: Un molde en el cual se adaptarán todos los Pokemon que construyamos. Hay que programarlos a todos! (de forma inteligente)

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Revisa el código Pokemon.java dentro de app/java/com.bedu.pokedex/Pokemon
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
