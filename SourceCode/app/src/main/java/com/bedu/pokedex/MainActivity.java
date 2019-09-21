60a3cf5858c17b50207219798e0152f937214132
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 16:26:29 -0500

    En este ejemplo muestro que pasa si pones varios objetos (textviews) dentro de un Linear Layout, puedes editar las propiedades y ver como funcionan y se comportan en la ventana de Preview (a tu derecha.)
    Nota: Ahora mismo por claridad dejé en Android: text el texto "hard-coded"
    pero en los siguientes ejemplos los pondré en res/value/strings.xml

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Revisa el activity_main.xml para ver que ocurre con este ejemplo.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
