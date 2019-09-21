293d3a870c5a86563aef94b571d8b3ac8880dd34
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 18:29:57 -0500

    Esta es la solucion al reto 1 ¿Lograste llegar a este resultado?

---
+++ SourceCode/app/src/main/java/com/bedu/pokedex/Pokemon.java
@@ -0,0 +1,16 @@
+package com.bedu.pokedex;
+
+/**
+ * Pokemon.java :D
+ */
+public class Pokemon {
+    //Aqui escribiremos el programa que va a controlar a todos los Pokemon.
+    //Primero todos los datos sobre un pokemon.
+
+    int numero;             //Numero (valor entero)
+    String nombre;          //Nombre (Cadena de caracteres)
+    double altura;          //Altura (valor de punto flotante)
+    double peso;            //Peso (valor de punto flotante)
+    String descripcion;     //Descripción (cadena de caracteres)
+    int tipo;               //Tipo del pokemon (valor entero).
+}
