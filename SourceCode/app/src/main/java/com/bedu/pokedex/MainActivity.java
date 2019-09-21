33566f0b13c0cebc668fa7e05c4118c4d5a5e197
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 15:40:27 -0500

    En este reto tu deber será crear Recursos para las carpetas:         drawable-xxhdpi         drawable-xhdpi         drawable-hdpi         drawable-mdpi         drawable-ldpi
        Ya he puesto el ejemplo creando la carpeta "drawable-xxxhdpi", tu reto ahora
        será tomar las imágenes y redimensionarlas con GIMP o con un script, como tu prefieras.
    
        Tips:
            1.- Las imágenes se encuentran dentro de res/drawable en la vista de "Android"
    
            2.- Puedes ver las carpetas en la vista "Project" (del lado superior izquierdo). La ruta es
                    app -> src -> main -> res, y dentro está drawable-xxxhdpi.
    
            3.- Puedes acceder a las imágenes dando click derecho en la imagen -> Show on Explorer
    
            4.- Recuerda crear las carpetas antes de tratar de insertar las nuevas imágenes.
         Buena suerte!

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -6,6 +6,29 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    En este reto tu deber será crear Recursos para las carpetas:
+        drawable-xxhdpi
+        drawable-xhdpi
+        drawable-hdpi
+        drawable-mdpi
+        drawable-ldpi
+
+    Ya he puesto el ejemplo creando la carpeta "drawable-xxxhdpi", tu reto ahora
+    será tomar las imágenes y redimensionarlas con GIMP o con un script, como tu prefieras.
+
+    Tips:
+        1.- Las imágenes se encuentran dentro de res/drawable en la vista de "Android"
+
+        2.- Puedes ver las carpetas en la vista "Project" (del lado superior izquierdo). La ruta es
+                app -> src -> main -> res, y dentro está drawable-xxxhdpi.
+
+        3.- Puedes acceder a las imágenes dando click derecho en la imagen -> Show on Explorer
+
+        4.- Recuerda crear las carpetas antes de tratar de insertar las nuevas imágenes.
+     Buena suerte!
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
