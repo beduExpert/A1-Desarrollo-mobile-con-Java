c05554c05057d64c2ff33a6237987deab62b468a
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 20:00:04 -0500

    En este reto agregamos Logs en toda la App. ¿Que tal quedó tu reto?

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -3,12 +3,19 @@ package com.bedu.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity","La actividad se va a crear, y un pokemon va a surgir de ella.");
+        Pokemon pikachu = new Pokemon(25,"Pikachu", 0.25,0.5,"Es un detective",5, 50);
+        pikachu.atacar("Trueno");
+        pikachu.recibirDano(5);
+        Log.d("MainActivity","La actividad se acaba de crear y está lista!");
     }
 }
