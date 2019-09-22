b334669630a4f07feab109ba6bc322ae2ba89758
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 19:45:57 -0500

    Este reto consistía en crear paquetes nuevos para organizar tus objetos Java. he creado los paquetes com.bedu.remote.maps y com.bedu.remote.messenger has podido crear paquetes en tu reto?

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Este reto consistía en crear paquetes nuevos para organizar tus objetos Java.
+    //he creado los paquetes com.bedu.remote.maps y com.bedu.remote.messenger
+    //has podido crear paquetes en tu reto?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
