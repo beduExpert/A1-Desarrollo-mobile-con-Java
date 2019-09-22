4e5e6c171833ac0b66d4252c47f79b8a59b86467
antonioarista <antonio.arista@gintelligence.mx>
2019-09-21 19:12:42 -0500

    Este es un ejemplo de la herencia de clases. Revisa también MainActivity.java para ver como se comporta la herencia.

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -10,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Así se implementa un pokemon volador.
+        PokemonVolador charizard = new PokemonVolador(3,"Charizard",100,3.0,"La descripción de Charizard va aquí",6,150);
+
+        //Charizard puede atacar y recibir daño
+        charizard.atacar("Llamarada");
+        charizard.recibirDano(10);
+
+        //Charizard ahora puede volar también
+        charizard.volar();
     }
 }
