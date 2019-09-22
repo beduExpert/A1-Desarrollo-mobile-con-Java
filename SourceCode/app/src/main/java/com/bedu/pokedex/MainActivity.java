b0a5417f3a102801dbb8bf2dadeed8e41c5ff9f5
antonioarista <antonio.arista@gintelligence.mx>
2019-09-22 01:22:10 -0500

    En este reto vamos a llamar via teléfono. Este código puede funcionar con Apps que puedan llamar como Skype.

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
