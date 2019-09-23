b8e41cde82238fa49167ee9c57dff7b80a337294
wicho1001 <wichoguerrero@yahoo.com>
2019-09-23 11:02:32 -0500

    Feat (Sesion-04/Ejemplo-02):Agregando código del Ejemplo-02 de la Sesion-04

---
+++ Sesion-04/Ejemplo-02/app/src/main/java/com/bedu/pokedex/Pokemon.java
@@ -0,0 +1,17 @@
+package com.bedu.pokedex;
+
+/**
+ * Este es el segundo ejemplo de la sesión 4.
+ * Aquí colocaremos todos los atributos
+ */
+public class Pokemon {
+    //Aqui escribiremos el programa que va a controlar a todos los Pokemon.
+    //Primero todos los datos sobre un pokemon.
+
+    int numero;             //Numero (valor entero)
+    String nombre;          //Nombre (Cadena de caracteres)
+    double altura;          //Altura (valor de punto flotante)
+    double peso;            //Peso (valor de punto flotante)
+    String descripcion;     //Descripción (cadena de caracteres)
+    int tipo;               //Tipo del pokemon (valor entero).
+}
