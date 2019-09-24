3a081bd71a6c3259dac7d39dc64668314835293c
wicho1001 <wichoguerrero@yahoo.com>
2019-09-24 10:36:05 -0500

    Feat(Sesion06/Ejemplo-03): Agregando código de Ejemplo-03 a Sesión 6

---
+++ Sesion-06/Ejemplo-03/app/src/main/java/com/bedu/pokedex/ColoresActivity.java
@@ -0,0 +1,18 @@
+package com.bedu.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColoresActivity extends AppCompatActivity {
+
+    /*
+    EN este ejemplo vamos a crear un frame layout y dos layouts dentro, para ver como funciona
+    el eje Z en las apps de Android. revisa el XML
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colores);
+    }
+}
