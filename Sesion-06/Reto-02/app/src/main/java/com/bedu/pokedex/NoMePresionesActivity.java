0420b2536bc93120af006607d3e9d18584c4fe14
wicho1001 <wichoguerrero@yahoo.com>
2019-09-24 10:36:41 -0500

    Feat(Sesion06/Reto-02): Agregando código de Reto-02 a Sesión 6

---
+++ Sesion-06/Reto-02/app/src/main/java/com/bedu/pokedex/NoMePresionesActivity.java
@@ -0,0 +1,59 @@
+package com.bedu.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NoMePresionesActivity extends AppCompatActivity {
+
+    /*
+    En este ejemplo vamos a vincular el archivo activity_no_me_presiones.xml con este código
+    Java. Dentro del XML hay dos objetos etiquetados: Un botón y un textview.
+     */
+    Button btn_NoMePresiones;
+    TextView txt_Mensaje;
+
+    ArrayList<String> mensajesGraciosos;
+    int contadorDeMensajes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_me_presiones);
+
+        //En este punto, unimos el objeto btn_NoMePresiones y txt_Mensaje con el archivo XML:
+        btn_NoMePresiones = findViewById(R.id.NoMePresiones_Button);
+        txt_Mensaje = findViewById(R.id.NoMePresiones_Text);
+
+        //Aqui, cambiamos el texto:
+        txt_Mensaje.setText("Que día tan tranquilo!");
+
+        mensajesGraciosos = new ArrayList<>();
+        agregarMensajesGraciosos();
+        contadorDeMensajes = 0;
+    }
+
+    private void agregarMensajesGraciosos()
+    {
+        mensajesGraciosos.add("Hey...");
+        mensajesGraciosos.add("Oye");
+        mensajesGraciosos.add("Basta!");
+        mensajesGraciosos.add("¿No tienes nada mejor que hacer?");
+        mensajesGraciosos.add("Hey, mira! una notificación!");
+        mensajesGraciosos.add("...");
+        mensajesGraciosos.add("Deja de presionar mi botón");
+        mensajesGraciosos.add("Detente!");
+        mensajesGraciosos.add("Quizás si dejo de hacerte caso dejarás de presionarlo...");
+        mensajesGraciosos.add("...");
+        mensajesGraciosos.add("...");
+        mensajesGraciosos.add("...");
+        mensajesGraciosos.add("Oh, ¿sigues aquí?");
+
+
+    }
+}
