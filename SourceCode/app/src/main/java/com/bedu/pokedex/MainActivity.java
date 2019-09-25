c3ed317ca05262df081f174927fa2d5dedb3565e
antonioarista <antonio.arista@gintelligence.mx>
2019-09-25 16:46:46 -0500

    En este ejemplo llamamos via telefónica

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -2,7 +2,12 @@ package com.bedu.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAEnviar = findViewById(R.id.btnLlamar);
+        final EditText telefono = findViewById(R.id.txtTelefono);
+        btnAEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String telefonoALlamar = telefono.getText().toString();
+                Intent llamadaIntent = new Intent(Intent.ACTION_DIAL);
+                llamadaIntent.setData(Uri.parse("tel:"+telefonoALlamar));
+                startActivity(llamadaIntent);
+            }
+        });
     }
 }
