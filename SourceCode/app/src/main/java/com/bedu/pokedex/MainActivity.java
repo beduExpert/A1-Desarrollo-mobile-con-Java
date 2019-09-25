aa5b6a9fb273ac0b7b195dbd4dea9aa66865a0a2
antonioarista <antonio.arista@gintelligence.mx>
2019-09-25 15:24:04 -0500

    En este ejemplo vemos como funciona la librería Math con algunas funciones básicas.

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -3,6 +3,12 @@ package com.bedu.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.floor;
+import static java.lang.Math.pow;
+import static java.lang.Math.sin;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        double resultado;
+
+        resultado = pow(5,2);
+        Log.d("Ejemplo","el resultado de 5 elevado al cuadrado es:"+resultado);
+
+        resultado = floor(5.2);
+        Log.d("Ejemplo","el resultado de 5.2 redondedo hacia abajo es:"+resultado);
+
+        resultado = sin(45);
+        Log.d("Ejemplo","el seno de 45 es:"+resultado);
+
+        resultado = cos(45);
+        Log.d("Ejemplo","el coseno de 45 es:"+resultado);
     }
 }
