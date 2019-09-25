088cb2bd86d7e69fc49da7b78fd7fba870f5f54e
antonioarista <antonio.arista@gintelligence.mx>
2019-09-25 16:31:29 -0500

    En este ejemplo enviamos un correo electronico.

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -2,7 +2,12 @@ package com.bedu.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAEnviar = findViewById(R.id.btnEnviame);
+        final EditText txtAEnviar = findViewById(R.id.mensajeTxt);
+        final EditText txtCorreo = findViewById(R.id.correoTxt);
+        btnAEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String mensajeAEnviar = txtAEnviar.getText().toString();
+                String direccionCorreo = txtCorreo.getText().toString();
+                String UrlCoreo = "mailto:"+direccionCorreo+"?"+
+                        "body="+ Uri.encode(mensajeAEnviar);
+                Intent intentACorreo = new Intent(Intent.ACTION_SENDTO);
+                intentACorreo.setData(Uri.parse(UrlCoreo));
+                startActivity(intentACorreo);
+            }
+        });
     }
 }
