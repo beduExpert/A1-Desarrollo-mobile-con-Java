f9e8b911f5066ba24cf2e7b3e8c4f993f6a204fd
antonioarista <antonio.arista@gintelligence.mx>
2019-09-25 15:17:06 -0500

    En este ejemplo se muestra como funciona el objeto Calendar en Java

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -3,6 +3,9 @@ package com.bedu.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Este es un ejemplo de como trabaja el objeto Calendar de Android:
+        int anio = Calendar.getInstance().get(Calendar.YEAR);
+        Log.d("Ejemplo", "El año actual es: "+anio);
+        int mes = Calendar.getInstance().get(Calendar.MONTH)+1;
+        Log.d("Ejemplo", "El mes actual es: "+mes);
+        int dia = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        Log.d("Ejemplo", "El dia actual es:"+dia);
     }
 }
