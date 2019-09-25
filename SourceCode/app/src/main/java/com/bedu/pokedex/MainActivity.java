07f2df2c06c54f0c78d4308167721a862264002a
antonioarista <antonio.arista@gintelligence.mx>
2019-09-25 15:33:32 -0500

    En este ejemplo vemos como se generan números aleatorios en Java

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -3,6 +3,9 @@ package com.bedu.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+        double aleatorio = rand.nextDouble();
+        Log.d("Ejemplo", "El valor aleatorio que generas es: "+aleatorio);
+
+        double minimo = 5;
+        double maximo = 105;
+        aleatorio = minimo + (maximo - minimo) * rand.nextDouble();
+        Log.d("Ejemplo", "El valor aleatorio entre "+minimo+" y "+maximo+" es: "+aleatorio);
+
     }
 }
