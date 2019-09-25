a3285f0e53a56677c8f651a900daa12b753f01a3
antonioarista <antonio.arista@gintelligence.mx>
2019-09-25 16:21:37 -0500

    En este ejemplo vamos a generar Intents hacia otra App.

--- SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
+++ SourceCode/app/src/main/java/com/bedu/pokedex/MainActivity.java
@@ -2,7 +2,11 @@ package com.bedu.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAEnviar = findViewById(R.id.btnEnviar);
+        final EditText txtAEnviar = findViewById(R.id.txtAEnviar);
+        btnAEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mensajeAEnviar = txtAEnviar.getText().toString();
+                Intent intentAOtraApp = new Intent(Intent.ACTION_SEND);
+                intentAOtraApp.setType("text/plain");
+                intentAOtraApp.putExtra(Intent.EXTRA_TEXT, mensajeAEnviar);
+                Intent pantallaSelectora = Intent.createChooser(intentAOtraApp, "Envia este mensaje por...");
+                startActivity(pantallaSelectora);
+            }
+        });
     }
 }
